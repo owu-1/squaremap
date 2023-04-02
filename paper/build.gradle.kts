419dc07c782f9333eb52d32bbb90d2f7a92966db
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2023-04-01 18:54:35 -0700

    Initial support for Folia

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -1,9 +1,10 @@
 import io.papermc.hangarpublishplugin.model.Platforms
+import xyz.jpenilla.runpaper.task.RunServer
+import xyz.jpenilla.runtask.service.DownloadsAPIService
 
 plugins {
   id("platform-conventions")
   id("io.papermc.paperweight.userdev")
-  id("net.minecrell.plugin-yml.bukkit")
   id("xyz.jpenilla.run-paper")
   id("io.papermc.hangar-publish-plugin")
 }
@@ -11,7 +12,7 @@ plugins {
 val minecraftVersion = libs.versions.minecraft
 
 dependencies {
-  paperweight.paperDevBundle(minecraftVersion.map { "$it-R0.1-SNAPSHOT" })
+  paperweight.devBundle("dev.folia", minecraftVersion.map { "$it-R0.1-SNAPSHOT" }.get())
 
   implementation(projects.squaremapCommon)
 
@@ -42,18 +43,34 @@ tasks {
   reobfJar {
     outputJar.set(productionJarLocation(minecraftVersion))
   }
+  processResources {
+    val props = mapOf(
+      "version" to project.version,
+      "website" to providers.gradleProperty("githubUrl").get(),
+      "description" to project.description,
+      "apiVersion" to minecraftVersion.get().take(4),
+    )
+    inputs.properties(props)
+    filesMatching("plugin.yml") {
+      expand(props)
+    }
+  }
 }
 
 squaremapPlatform {
   productionJar.set(tasks.reobfJar.flatMap { it.outputJar })
 }
 
-bukkit {
-  main = "xyz.jpenilla.squaremap.paper.SquaremapPaperBootstrap"
-  name = rootProject.name
-  apiVersion = "1.19"
-  website = providers.gradleProperty("githubUrl").get()
-  authors = listOf("jmp")
+val foliaService = DownloadsAPIService.registerIfAbsent(project) {
+  downloadsEndpoint = "https://api.papermc.io/v2/"
+  downloadProjectName = "folia"
+}
+
+tasks.register<RunServer>("runFolia") {
+  downloadsApiService.set(foliaService)
+  version.set(minecraftVersion)
+  pluginJars.from(squaremapPlatform.productionJar)
+  group = "run paper"
 }
 
 hangarPublish.publications.register("plugin") {
